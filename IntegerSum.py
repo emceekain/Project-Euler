6f4cf4d5abb173646761b7a273a9a18967dbdd87
Brent McKain <emceekain@gmail.com>
2016-10-29 00:04:29 -0500

    Update IntegerSum.py

--- IntegerSum.py
+++ IntegerSum.py
@@ -2,7 +2,7 @@ def IntegerSum(n):
 	sum = int(n*(n+1)/2)
 	return sum
 
-#Makes fib usable as  script
+#Makes IntegerSum usable as  script
 if __name__=="__main__":
 	import sys
 	IntegerSum(int(sys.argv[1]))
